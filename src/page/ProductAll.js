b43c0bd017faaea13972899ac3c84a534cf6b453
SangJee Park <sjpark3927@gmail.com>
2024-09-30 02:23:46 +0900

    'json'

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -6,7 +6,7 @@ const ProductAll = ({authenticate}) => {
   const [productList, setProductList ] =useState([]);
 
   const getProducts= async()=>{
-    let url =`http://localhost:5000/products`
+    let url =` https://my-json-server.typicode.com/SJeePark/h-m/products`
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data)
